53c7dc2674abaebeb0f2804431f134ed791bdaef
Janne Mattila <janne.mattila@hotmail.fi>
2021-09-01 12:36:19 +0300

    Add deployment script placeholders

---
+++ deploy/deploy.ps1
@@ -0,0 +1,92 @@
+Param (
+    [Parameter(HelpMessage = "Deployment target resource group")] 
+    [string] $ResourceGroupName = "rg-logicapp-standard-demo",
+
+    [Parameter(HelpMessage = "Deployment target resource group location")] 
+    [string] $Location = "North Europe",
+
+    [Parameter(HelpMessage = "Logic App app Name")] 
+    [string] $LogicAppName = "contosologicapp-local",
+
+    [string] $Template = "azuredeploy.json",
+    [string] $TemplateParameters = "$PSScriptRoot\azuredeploy.parameters.json",
+    [switch] $PreserveDeploymentContainer
+)
+
+$ErrorActionPreference = "Stop"
+
+$date = (Get-Date).ToString("yyyy-MM-dd-HH-mm-ss")
+$deploymentName = "Local-$date"
+
+if ([string]::IsNullOrEmpty($env:GITHUB_SHA)) {
+    Write-Host (@"
+Not executing inside GitHub Action.
+Make sure you have done "Login-AzAccount" and
+"Select-AzSubscription -SubscriptionName name"
+so that script continues to work correctly for you.
+"@)
+}
+else {
+    $deploymentName = $env:GITHUB_SHA
+}
+
+# Target deployment resource group
+if ($null -eq (Get-AzResourceGroup -Name $ResourceGroupName -Location $Location -ErrorAction SilentlyContinue)) {
+    Write-Warning "Resource group '$ResourceGroupName' doesn't exist and it will be created."
+    New-AzResourceGroup -Name $ResourceGroupName -Location $Location -Verbose
+}
+
+# Additional parameters that we pass to the template deployment
+$additionalParameters = New-Object -TypeName hashtable
+$additionalParameters['logicAppName'] = $LogicAppName
+
+$result = New-AzResourceGroupDeployment `
+    -DeploymentName $deploymentName `
+    -ResourceGroupName $ResourceGroupName `
+    -TemplateFile $Template `
+    -TemplateParameterFile $TemplateParameters `
+    @additionalParameters `
+    -Mode Complete -Force `
+    -Verbose
+
+if ($null -eq $result.Outputs.apimGateway) {
+    Throw "Template deployment didn't return web app information correctly and therefore deployment is cancelled."
+}
+
+$result | Select-Object -ExcludeProperty TemplateLinkString
+
+$apimGateway = $result.Outputs.apimGateway.value
+
+# Publish variable to the Azure DevOps agents so that they
+# can be used in follow-up tasks such as application deployment
+Write-Host "##vso[task.setvariable variable=Custom.APIMGateway;]$apimGateway"
+
+Write-Host "Smoke testing that our *MANDATORY* API is up and running..."
+$webAppUri = "$apimGateway/users"
+$data = @{
+    id      = 1
+    name    = "Doe"
+    address = @{
+        street     = "My street 1"
+        postalCode = "12345"
+        city       = "My city"
+        country    = "My country"
+    }
+}
+$body = ConvertTo-Json $data
+for ($i = 0; $i -lt 60; $i++) {
+    try {
+        $request = Invoke-WebRequest -Body $body -ContentType "application/json" -Method "POST" -DisableKeepAlive -Uri $webAppUri -ErrorAction SilentlyContinue
+        Write-Host "API status code $($request.StatusCode)."
+
+        if ($request.StatusCode -eq 200) {
+            Write-Host "API is up and running."
+            return
+        }
+    }
+    catch {
+        Start-Sleep -Seconds 3
+    }
+}
+
+Throw "Mandatory API didn't respond on defined time."
---
+++ deploy/main.bicep
@@ -0,0 +1,101 @@
+param logicAppName string
+
+param appInsightsName string = 'la-ai'
+param storageName string = logicAppName
+
+resource appInsights 'Microsoft.Insights/components@2020-02-02' = {
+  name: appInsightsName
+  location: resourceGroup().location
+  kind: 'web'
+  properties: {
+    Application_Type: 'web'
+  }
+}
+
+resource storageaccount 'Microsoft.Storage/storageAccounts@2021-02-01' = {
+  name: storageName
+  location: resourceGroup().location
+  kind: 'StorageV2'
+  sku: {
+    name: 'Standard_LRS'
+  }
+  properties: {
+    supportsHttpsTrafficOnly: true
+    minimumTlsVersion: 'TLS1_2'
+  }
+}
+
+resource appServicePlan 'Microsoft.Web/serverfarms@2020-12-01' = {
+  name: 'workflowPlan'
+  location: resourceGroup().location
+  sku: {
+    name: 'WS1'
+    capacity: 1
+  }
+}
+
+resource azureFunction 'Microsoft.Web/sites@2020-12-01' = {
+  name: logicAppName
+  location: resourceGroup().location
+  kind: 'functionapp,workflowapp'
+  identity: {
+    type: 'SystemAssigned'
+  }
+  properties: {
+    serverFarmId: appServicePlan.id
+    clientAffinityEnabled: false
+    httpsOnly: true
+    siteConfig: {
+      appSettings: [
+        {
+          name: 'AzureWebJobsDashboard'
+          value: 'DefaultEndpointsProtocol=https;AccountName=${storageaccount.name};AccountKey=${listKeys(storageaccount.id, '2019-06-01').key1}'
+        }
+        {
+          name: 'AzureWebJobsStorage'
+          value: 'DefaultEndpointsProtocol=https;AccountName=${storageaccount.name};AccountKey=${listKeys(storageaccount.id, '2019-06-01').key1}'
+        }
+        {
+          name: 'WEBSITE_CONTENTAZUREFILECONNECTIONSTRING'
+          value: 'DefaultEndpointsProtocol=https;AccountName=${storageaccount.name};AccountKey=${listKeys(storageaccount.id, '2019-06-01').key1}'
+        }
+        {
+          name: 'WEBSITE_CONTENTSHARE'
+          value: 'content'
+        }
+        {
+          name: 'FUNCTIONS_EXTENSION_VERSION'
+          value: '~3'
+        }
+        {
+          name: 'FUNCTIONS_WORKER_RUNTIME'
+          value: 'node'
+        }
+        {
+          name: 'WEBSITE_NODE_DEFAULT_VERSION'
+          value: '~12'
+        }
+        {
+          name: 'APPINSIGHTS_INSTRUMENTATIONKEY'
+          value: appInsights.properties.InstrumentationKey
+        }
+        {
+          name: 'APPLICATIONINSIGHTS_CONNECTION_STRING'
+          value: appInsights.properties.ConnectionString
+        }
+        {
+          name: 'AzureFunctionsJobHost__extensionBundle__id'
+          value: 'Microsoft.Azure.Functions.ExtensionBundle.Workflows'
+        }
+        {
+          name: 'AzureFunctionsJobHost__extensionBundle__version'
+          value: '[1.*, 2.0.0)'
+        }
+        {
+          name: 'APP_KIND'
+          value: 'workflowApp'
+        }
+      ]
+    }
+  }
+}
